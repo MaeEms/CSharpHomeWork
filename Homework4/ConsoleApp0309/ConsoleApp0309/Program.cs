5e8a0fd630e3e01c3c1bcf5579def95572c3bc6a
MaeEms <751861005@qq.com>
2020-03-09 16:52:36 +0800

    This is my forth homework

---
+++ Homework4/ConsoleApp0309/ConsoleApp0309/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0309
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> mylist = new GenericList<int>();
+            for (int i= 1; i <=10 ; i++)
+            {
+                mylist.Add(i);
+            }
+
+            mylist.ForEach(x => Console.WriteLine(x));
+
+            int max = mylist.Head.Data;
+            mylist.ForEach(x => max = x > max ? x : max);
+            
+            int min = mylist.Head.Data;
+            mylist.ForEach(x => min = x < min ? x : min);
+            
+            int num = 0;
+            mylist.ForEach(x => num += x);
+            Console.WriteLine($"最大值為{max}，最小值為{min}，合為{num}。");
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public  Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> flag = head;
+            while (head.Next != null)
+            {
+                action(flag.Data);
+                flag = flag.Next;
+            }
+        }
+    }
+}
---
+++ Homework4/ConsoleApp0309_02/ConsoleApp0309_02/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp0309_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("設定的時間為2020-03-09 16:45:00\n");
+           
+            //該操設置時間           
+            DateTime dt = DateTime.Now;
+            string strNow = dt.ToString("2020-03-09 16:49:00");
+            dt = DateTime.Parse(strNow);
+
+            Form form = new Form();
+            form.clock.work(dt);
+            Console.ReadKey();
+
+        }
+    }
+
+    //使用事件机制，模拟实现一个闹钟功能。
+    //闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+    //在闹钟走时时或者响铃时，在控制台显示提示信息。
+    public delegate void ClickHandler();
+    
+    public class myClock
+    {
+        public event ClickHandler Tick;
+        public event ClickHandler Alarm;
+
+        public void work(DateTime time)
+        {
+            bool flag=false;
+            while (true)
+            {
+                if(DateTime.Now < time||flag)
+                {
+                    Tick();
+                    Thread.Sleep(1000);
+                }
+                else {
+                    Alarm();
+                    flag = true;
+                    Thread.Sleep(1000);
+                }
+                
+            }
+           
+        }
+
+    }
+
+    public class Form
+    {
+        public myClock clock = new myClock();
+
+        public Form()
+        {
+            clock.Tick += clockTick;
+            clock.Alarm += clockAlarm;
+
+        }
+
+        void clockTick()
+        {
+            Console.Write("Tick\n");
+        }
+
+        void clockAlarm()
+        {
+            Console.Write("Ding~~~!!!!\n");
+        }
+    }
+}
