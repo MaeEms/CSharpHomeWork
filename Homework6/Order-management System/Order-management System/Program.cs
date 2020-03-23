9a699f0ac98a885df6750fdc793b6c167b754190
MaeEms <751861005@qq.com>
2020-03-24 01:18:54 +0800

    This is my sixth homework

---
+++ Homework6/Order-management System/Order-management System/Program.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Order_management_System
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //五种货物
+            Goods apple = new Goods("apple", 10.8);
+            Goods banana = new Goods("banana", 22.8);
+            Goods water = new Goods("water", 9.8);
+            Goods cake = new Goods("cake", 49.99);
+            Goods pencil = new Goods("pencil", 5.0);
+            //四位顾客
+            Customer Mike = new Customer("Mike", "New York");
+            Customer Ammy = new Customer("Ammy", "London");
+            Customer LiHua = new Customer("LiHua", "Shanghai");
+            Customer Koya = new Customer("Koya", "Tokyo");
+
+            OrderService os = new OrderService();
+
+            Order order1 = new Order(1, Mike);
+            order1.AddGoods(apple, 5);
+            order1.AddGoods(banana, 3);
+            order1.AddGoods(cake, 1);
+            os.AddOrder(order1);
+
+            Order order2 = new Order(2, Koya);
+            order2.AddGoods(apple, 5);
+            order2.AddGoods(pencil, 6);
+            order2.AddGoods(water, 8);
+            os.AddOrder(order2);
+
+            Order order3 = new Order(3, LiHua);
+            order3.AddGoods(pencil, 3);
+           // order3.AddGoods(pencil, 4);     会抛异常：“货品添加重复”
+            order3.AddGoods(cake, 1);
+            order3.AddGoods(water, 2);
+            os.AddOrder(order3);
+
+            Order order4 = new Order(4, Ammy);
+            order4.AddGoods(apple, 7);
+            order4.AddGoods(pencil, 8);
+            order4.AddGoods(water, 3);
+            os.AddOrder(order4);
+
+            Order order5 = new Order(5, Ammy);
+            order5.AddGoods(cake, 2);
+            order5.AddGoods(pencil, 15);
+            order5.AddGoods(water, 3);
+            os.AddOrder(order5);
+
+            //展示所有订单
+            os.QueryOrders();
+            
+            //进行序列化
+            os.Export("Orders");
+            Console.WriteLine("\n将所有订单序列化");
+            Console.WriteLine(File.ReadAllText("orders.xml"));
+            //将所有的订单删除
+            Console.WriteLine("将除订单1以外的所有订单删除\n");
+            os.DeleteOrderByID(2);
+            os.DeleteOrderByID(3);
+            os.DeleteOrderByID(4);
+            os.DeleteOrderByID(5);
+            Console.WriteLine("现在还剩的订单是");
+            os.QueryOrders();
+            //将存在orders.xml中的订单导入
+            Console.WriteLine("将所有订单导入\n");
+            os.Import("Orders");
+            os.QueryOrders();
+            Console.ReadKey();
+            
+
+
+        }
+    }
+
+    [Serializable]
+     public class Goods
+    {
+        public string GoodsName { set; get; }
+        public double GoodsPrice { set; get; }
+
+        public Goods() { }
+        public Goods(string name,double price)
+        {
+            GoodsName = name;
+            GoodsPrice = price;
+        }
+        public override string ToString()
+        {
+            return $"{GoodsName}   ${GoodsPrice}";
+        }
+    }
+
+    [Serializable]
+   public class Customer
+    {
+        public string name { set; get; }
+        public string address { set; get; }
+
+        public Customer() { }
+        public Customer(string name,string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public override string ToString()
+        {
+            return $"{name} {address}";
+        }
+    }
+
+    [Serializable]
+    public class OrderItem
+    {
+        public Goods Good { set; get; }
+        public int Count { set; get; }
+
+        public OrderItem() { }
+        public OrderItem(Goods Good,int Count)
+        {
+            this.Good = Good;
+            this.Count = Count;
+        }
+
+        public override bool Equals(object obj)
+        {
+            OrderItem oi = obj as OrderItem;
+            if (oi == null) return false;
+            else return Good.GoodsName == oi.Good.GoodsName;
+        }
+
+        public override int GetHashCode()
+        {
+            return Good.GetHashCode()+100*Count;
+        }
+
+        public double GetCount()
+        {
+            return Good.GoodsPrice* Count;
+        }
+        public override string ToString()
+        {
+            
+            return $"{Good}     {Count}     {GetCount()}";
+        }
+    }
+
+    [Serializable]
+    public class Order
+    {
+        public int ID { set; get; }
+        public List<OrderItem> Goods { set; get; }
+        public Customer Customer { set; get; }
+        public DateTime Time { set; get; }
+
+        public Order() { }
+
+        public Order(int ID, Customer Customer) 
+        {
+            this.ID = ID;
+            Goods = new List<OrderItem>();
+            this.Customer = Customer;
+            Time = DateTime.Now;
+        }
+        public void AddGoods(Goods goods,int count)
+        {
+            OrderItem oi = new OrderItem(goods, count);
+            var query = from s in Goods
+                        where s.Equals(oi)
+                        select s;
+
+            if (!query.Any()) Goods.Add(oi);
+            else throw new Exception("添加货品重复");
+        }
+        public override bool Equals(Object obj)
+        {
+            Order or = obj as Order;
+            if (or == null) return false;
+            else return ID == or.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID;
+        }
+
+        public double GetCount()
+        {
+            double Count = 0;
+            foreach(OrderItem oi in Goods)
+            {
+                Count += oi.GetCount();
+            }
+            return Count;
+        }
+        public override string ToString()
+        {
+            
+            string order = $"ID：{ID}             \n";
+                order += "--------------------------------\n";
+               order += $"货物    单价    数量     合计\n";
+                foreach(OrderItem o in Goods)
+            {
+               order += $"{o}                      \n";
+            }
+                order += "--------------------------------\n";
+               order += $"总价为 ${GetCount()}     \n";
+                order += $"{Customer}\n";
+                order += $"{Time}\n";
+                order += "-------------------------------\n";
+
+            return order;
+
+        }
+    }
+
+    public class OrderService
+    {
+        public List<Order> Orders { set; get; }
+
+        public OrderService()
+        {
+            Orders = new List<Order>();
+        }
+
+        //添加一张账单
+        public bool AddOrder(Order o)
+        {
+            if (o == null) return false;
+            if (Orders.Contains(o)) return false;
+            else Orders.Add(o);
+            return true;
+
+        }
+
+        //两种删除账单的方式
+        //根据账单ID删除一张账单
+        public bool DeleteOrderByID(int id)
+        {
+            int index = Orders.FindIndex(o => o.ID == id);
+            if (index == -1)
+                return false;
+            Orders.RemoveAt(index);
+            return true;
+        }
+        //删除某个人的全部账单
+        public bool DeleteCustomerOrder(Customer c)
+        {
+            int index = Orders.FindIndex(o => o.Customer == c);
+            if (index == -1)
+                return false;
+            Orders.RemoveAt(index);
+            return true;
+        }
+
+
+        //更改订单：这里提供了两种方式
+        //更改一个订单的订单项
+        public bool ChangeOrderItem(int id, List<OrderItem> oilist)
+        {
+            int index = Orders.FindIndex(o => o.ID == id);
+            if (index == -1)
+                return false;
+            else Orders[index].Goods = oilist;
+            return true;
+
+        }
+        //更改一个订单的顾客
+        public bool ChangeOrderCustomerAndAddress(int id, Customer c)
+        {
+            if (c == null) return false;
+            int index = Orders.FindIndex(o => o.ID == id);
+            if (index == -1)
+                return false;
+            else Orders[index].Customer = c;
+            return true;
+        }
+
+
+        /* 查询订单：这里提供了三种方式*/
+        //查询某个订单的内容
+        public bool QueryOrder(int id)
+        {
+            int index = Orders.FindIndex(o => o.ID == id);
+            if (index == -1)
+                return false;
+            else Console.WriteLine(Orders[index]);
+            return true;
+
+        }
+
+        //查询所有的订单
+        public bool QueryOrders()
+        {
+            if (Orders.ToArray().Length == 0) return false;
+            IEnumerable<Order> query = from o in Orders 
+                                        orderby o.GetCount()
+                                        select o;
+            List<Order> Orders2 = query.ToList();
+            for (int i = 0; i < Orders2.ToArray().Length; i++)
+                Console.WriteLine(Orders2[i]);
+            return true;
+        }
+
+
+        //查询某个顾客的所有订单
+        public bool QueryOrderByCustomer(Customer c)
+        {
+            bool flag = false;
+            if (c == null) return false;
+            if (Orders.ToArray().Length == 0) return false;
+            for (int i = 0; i < Orders.ToArray().Length; i++)
+                if (Orders[i].Customer.Equals(c))
+                {
+                    Console.WriteLine(Orders[i]);
+                    flag = true;
+                }
+
+            return flag;
+
+        }
+
+        //按照ID进行排序
+        public bool Sort()
+        {
+            if (Orders.ToArray().Length == 0) return false;
+            var query = from s in Orders
+                        orderby s.ID
+                        select s;
+            Orders = query.ToList();
+            return true;
+        }
+
+        public void display()
+        {
+            foreach (Order o in Orders)
+            {
+                Console.WriteLine(o);
+            }
+        }
+
+
+        //依据lambda表达式自定义排序
+        public bool Sort(Func<Order, Order, int> func)
+        {
+
+            if (Orders.ToArray().Length == 0) return false;
+            if (func == null) return false;
+            Orders.Sort((a, b) => func(a, b));
+            return true;
+
+        }
+
+
+        //将所有订单序列化为XML文件
+        public void Export(string pass)
+        {
+            XmlSerializer xmlserializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(pass + ".xml", FileMode.Create))
+                xmlserializer.Serialize(fs, Orders);
+
+        }
+
+        //将所有订单反序列化
+        public void Import(string pass)
+        {
+
+            XmlSerializer xmlserializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(pass + ".xml", FileMode.Open))
+                Orders = (List<Order>)xmlserializer.Deserialize(fs);
+
+        }
+
+    }
+
+    
+}
---
+++ Homework6/Order-management System/Order-management SystemTests/OrderServiceTests.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Order_management_System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_management_System.Tests
+{
+
+    
+
+    [TestClass()]
+    public class OrderServiceTests
+    {
+
+        OrderService os = new OrderService();
+
+        //五种货物
+        Goods apple = new Goods("apple", 10.8);
+        Goods banana = new Goods("banana", 22.8);
+        Goods water = new Goods("water", 9.8);
+        Goods cake = new Goods("cake", 49.99);
+        Goods pencil = new Goods("pencil", 5.0);
+        //四位顾客
+        Customer Mike = new Customer("Mike", "New York");
+        Customer Ammy = new Customer("Ammy", "London");
+        Customer LiHua = new Customer("LiHua", "Shanghai");
+        Customer Koya = new Customer("Koya", "Tokyo");
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Order order = new Order(1, Mike);
+            order.AddGoods(apple, 2);
+            order.AddGoods(banana, 3);
+            order.AddGoods(cake, 1);
+
+            os.AddOrder(order);
+        }
+        
+
+
+
+        //添加重复编号订单
+        [TestMethod()]
+        
+        public void AddOrderTest()
+        {
+            Order order = new Order(1, LiHua);
+            Assert.AreEqual(os.AddOrder(order), false);
+              
+        }
+
+        //加入一个空订单
+        [TestMethod()]
+        public void AddOrderTest2()
+        {
+            Order order = null;
+            Assert.AreEqual(os.AddOrder(order), false);
+
+        }
+
+        //正常情况希望加入成功
+        [TestMethod()]     
+        public void AddOrderTest3()
+        {
+            Order order1 = new Order(6, Mike);
+            os.AddOrder(order1);                                      
+            Assert.AreEqual( os.Orders.Contains(order1),true);
+            
+        }
+
+        //正常情况希望删除成功
+        [TestMethod()]
+        public void DeleteOrderByIDTest()
+        {
+            Assert.AreEqual(os.DeleteOrderByID(1), true);
+
+        }
+
+        //删除不存在的订单
+        [TestMethod()]
+        public void DeleteOrderByIDTest2()
+        {
+            Assert.AreEqual(os.DeleteOrderByID(8), false);
+
+        }
+
+        //正常情况希望根据顾客删除订单
+        [TestMethod()]
+        public void DeleteCustomerOrderTest()
+        {
+            Assert.AreEqual(os.DeleteCustomerOrder(Mike),true);
+        }
+
+        //顾客不存在
+        [TestMethod()]
+        public void DeleteCustomerOrderTest2()
+        {
+            Assert.AreEqual(os.DeleteCustomerOrder(Koya), false);
+        }
+
+        //正常情况希望成功改变表项
+        [TestMethod()]
+        public void ChangeOrderItemTest()
+        {
+            OrderItem oi1 = new OrderItem(pencil, 5);
+            OrderItem oi2 = new OrderItem(water, 3);
+            List<OrderItem> oi = new List<OrderItem>();
+            oi.Add(oi1);
+            oi.Add(oi2);
+            Assert.AreEqual(os.ChangeOrderItem(1, oi), true);
+
+
+        }
+
+        //正常情况希望成功改变顾客
+        [TestMethod()]
+        public void ChangeOrderCustomerAndAddressTest()
+        {
+            Assert.AreEqual(os.ChangeOrderCustomerAndAddress(1, Koya), true);
+
+         }
+
+        //改变的顾客不能为空
+        [TestMethod()]
+        public void ChangeOrderCustomerAndAddressTest2()
+        {
+            Assert.AreEqual(os.ChangeOrderCustomerAndAddress(1, null), false);
+
+        }
+
+        //订单不存在时
+        [TestMethod()]
+        public void QueryOrderTest()
+        {
+            Assert.AreEqual(os.QueryOrder(2), false);
+        }
+
+        //没有任何订单时
+        [TestMethod()]
+        public void QueryOrdersTest()
+        {                    
+            OrderService os2 = new OrderService();
+            Assert.AreEqual(os2.QueryOrders(), false);   
+        }
+
+        //没有这样顾客的订单时
+        [TestMethod()]
+        public void QueryOrderByCustomerTest()
+        {
+            Assert.AreEqual(os.QueryOrderByCustomer(Ammy),false);
+
+        }
+
+        //顾客为空值时
+        [TestMethod()]
+        public void QueryOrderByCustomerTest2()
+        {
+            Mike = null;
+            Assert.AreEqual(os.QueryOrderByCustomer(Mike), false);
+
+        }
+
+        //正常情况，希望按照id排序
+        [TestMethod()]
+        public void SortTest()
+        {
+            OrderService os2 = new OrderService();
+            Order o1 = new Order(1, Mike); os2.AddOrder(o1);
+            Order o3 = new Order(3, Koya); os2.AddOrder(o3);
+            Order o2 = new Order(2, Ammy); os2.AddOrder(o2);
+            os2.Sort();
+            Assert.AreEqual(os2.Orders[1].ID==o2.ID, true);
+            
+        }
+        
+
+      
+        //正常情况希望按照id排序
+        [TestMethod()]
+        public void SortTest1()
+        {
+            OrderService os2 = new OrderService();
+            Order o1 = new Order(1, Mike); os2.AddOrder(o1);
+            Order o3 = new Order(3, Koya); os2.AddOrder(o3);
+            Order o2 = new Order(2, Ammy); os2.AddOrder(o2);
+            os2.Sort((a,b)=>a.ID-b.ID);
+            Assert.AreEqual(os2.Orders[1].ID == o2.ID, true);
+
+        }
+
+        //当lambda表达式为空
+        [TestMethod()]
+        public void SortTest2()
+        {
+            OrderService os2 = new OrderService();
+            Order o1 = new Order(1, Mike); os2.AddOrder(o1);
+            Order o3 = new Order(3, Koya); os2.AddOrder(o3);
+            Order o2 = new Order(2, Ammy); os2.AddOrder(o2);
+            os2.Sort(null);
+            Assert.AreEqual(os2.Orders[1].ID == o2.ID, false);
+        }
+
+
+        //成功序列化
+        [TestMethod()]
+        public void ExportTest()
+        {
+            os.Export("order");
+            FileInfo fileInfo = new FileInfo("order.xml");
+            Assert.IsTrue(fileInfo.Exists);
+        }
+
+        
+        //成功反序列化
+        [TestMethod()]
+        public void ImportTest()
+        {
+            os.Export("test");
+            OrderService os2 = new OrderService();
+            os2.Import("test");
+            Assert.AreEqual(os.Orders.ToArray().Length, os2.Orders.ToArray().Length);
+            for(int i=0; i< os.Orders.ToArray().Length;i++)
+            Assert.AreEqual(os.Orders[i], os2.Orders[i]);
+        }
+
+    }
+}
